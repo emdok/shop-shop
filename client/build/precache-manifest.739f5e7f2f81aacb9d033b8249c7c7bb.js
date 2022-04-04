7abc7b4995491685ccd8267712cf35a621d9ce6f
Emily Dokken <dokkenemily@gmail.com>
2022-04-04 15:06:56 -0700

    add global state

---
+++ client/build/precache-manifest.739f5e7f2f81aacb9d033b8249c7c7bb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0444ea024a3272485603505d4cfee628",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76f79f1b0b9123db4c03",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "954caf182929c47e2c54",
+    "url": "/static/js/2.288c6b8b.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.288c6b8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76f79f1b0b9123db4c03",
+    "url": "/static/js/main.4bbf7d7e.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
